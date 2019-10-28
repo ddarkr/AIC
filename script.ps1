15f673622e2e77e284fb310ebc6a8819851af5aa
unknown <admin@ddark.kr>
2019-10-28 19:44:08 +0900

    edit: node.js lts

--- script.ps1
+++ script.ps1
@@ -4,7 +4,7 @@ $host.ui.RawUI.WindowTitle = 'AutoInstall-Chocolatey v' + $version # Set title
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
-choco install nodejs -y
+choco install nodejs-lts -y
 
 choco install vscode -y
 
