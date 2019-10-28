b2b70a6bbaf337d109a5da1513f206be77fe2b46
unknown <admin@ddark.kr>
2019-10-28 19:37:24 +0900

    version 0.1

---
+++ install.bat
@@ -0,0 +1,4 @@
+@echo off
+cls
+Powershell.exe -noprofile -executionpolicy bypass -file "./script.ps1"
+pause
---
+++ script.ps1
@@ -0,0 +1,13 @@
+$version = 0.1
+
+$host.ui.RawUI.WindowTitle = 'AutoInstall-Chocolatey v' + $version # Set title
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+
+choco install nodejs -y
+
+choco install vscode -y
+
+choco install chromium -y
+
+choco install git -y
