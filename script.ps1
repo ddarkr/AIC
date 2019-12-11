9a11d06726c8b7653718ba2395d709fba5892576
도다 <admin@ddark.kr>
2019-12-11 18:39:20 +0900

    add: PHP, Composer

--- script.ps1
+++ script.ps1
@@ -4,12 +4,4 @@ $host.ui.RawUI.WindowTitle = 'AutoInstall-Chocolatey v' + $version # Set title
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
-choco install nodejs-lts -y
-
-choco install vscode -y
-
-choco install googlechrome -y
-
-choco install git -y
-
-choco install phpstorm -y
+choco install nodejs-lts vscode googlechrome git phpstorm php composer -y
