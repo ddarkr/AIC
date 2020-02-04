cdd58b069db1da5841dc2891fd475b67fd09534e
도다 <ddark.kr@gmail.com>
2020-02-04 17:37:20 +0900

    fix: 🔥 SSL/TLS Error

--- script.ps1
+++ script.ps1
@@ -2,6 +2,8 @@ $version = 0.1
 
 $host.ui.RawUI.WindowTitle = 'AutoInstall-Chocolatey v' + $version # Set title
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 choco install nodejs-lts vscode googlechrome git phpstorm php composer -y
