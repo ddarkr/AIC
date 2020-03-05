749fba31635e89aa900bc2e1016f80e1aefffa7d
도다 <ddark.kr@gmail.com>
2020-03-05 15:32:11 +0900

    feat: ⚡ `googlechrome` 삭제

--- script.ps1
+++ script.ps1
@@ -6,4 +6,4 @@ $host.ui.RawUI.WindowTitle = 'AutoInstall-Chocolatey v' + $version # Set title
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
-choco install nodejs-lts vscode googlechrome git phpstorm php composer -y
+choco install nodejs-lts vscode git phpstorm php composer -y
