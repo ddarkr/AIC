4f010279f542963a03a5dc543d1011eb483c96b8
도다 <ddark.kr@gmail.com>
2020-07-10 09:05:15 +0900

    feat: `googlechromebeta` 추가

--- script.ps1
+++ script.ps1
@@ -6,4 +6,6 @@ $host.ui.RawUI.WindowTitle = 'AutoInstall-Chocolatey v' + $version # Set title
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
+choco install googlechromebeta --pre -y
+
 choco install nodejs-lts vscode git phpstorm php composer -y
